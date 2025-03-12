850ce8445c8dd7800a2d2c57bbae23bda3071d7f
Piotr Tomczewski <piotr.tomczewski@callstack.com>
2025-03-12 16:28:29 +0100

    refactor: rename project and update references from `BareExampleNotice` to `ReactNativeLegalBareExample` across iOS and Android implementations

--- examples/bare-example/android/app/src/main/java/com/bareexamplenotice/MainActivity.kt
+++ examples/bare-example/android/app/src/main/java/com/bareexamplenotice/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.bareexamplenotice
+package com.ReactNativeLegalBareExample
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "BareExampleNotice"
+  override fun getMainComponentName(): String = "ReactNativeLegalBareExample"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- examples/bare-example/android/app/src/main/java/com/bareexamplenotice/MainApplication.kt
+++ examples/bare-example/android/app/src/main/java/com/bareexamplenotice/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.bareexamplenotice;
+package com.ReactNativeLegalBareExample;
 
 import android.app.Application
 import com.facebook.react.PackageList
